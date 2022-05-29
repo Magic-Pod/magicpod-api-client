a3f61ea9c415da0727f414f26708917c97509405
Nozomi Ito <nozomi.ito@trident-qa.com>
2022-05-29 23:02:08 +0900

    bump up version to 0.99.3.1

--- magicpod-api-client.go
+++ magicpod-api-client.go
@@ -12,7 +12,7 @@ import (
 
 func main() {
 	app := cli.NewApp()
-	app.Version = "0.95.0.1"
+	app.Version = "0.99.3.1"
 	app.Name = "magicpod-api-client"
 	app.Usage = "Simple and useful wrapper for MagicPod Web API"
 	app.Flags = []cli.Flag{
