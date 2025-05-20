1aac0b4f91292466b338dc90c0a01158ce968d3c
Miyuki Kondo <meowmeowcats0830v@gmail.com>
2025-05-20 15:35:00 +0900

    fix: option B to br

--- magicpod-api-client.go
+++ magicpod-api-client.go
@@ -33,7 +33,7 @@ func main() {
 					Usage: "Test settings number defined in the project batch run page",
 				},
 				cli.StringFlag{
-					Name:  "branch_name, B",
+					Name:  "branch_name, br",
 					Usage: "Branch name",
 				},
 				cli.StringFlag{
